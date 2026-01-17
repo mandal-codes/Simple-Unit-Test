b537e21454d6074831a3f02b392614bcb2e219e4
Shubham Mandal <mandal.shubham.learn@gmail.com>
2026-01-17 12:07:03 +0530

    Initial Commit

---
+++ Simple Unit Test/Simple_Unit_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Unit_TestApp.swift
+//  Simple Unit Test
+//
+//  Created by Shubham Mandal on 17/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Unit_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Simple Unit TestTests/Simple_Unit_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Unit_TestTests.swift
+//  Simple Unit TestTests
+//
+//  Created by Shubham Mandal on 17/01/26.
+//
+
+import Testing
+@testable import Simple_Unit_Test
+
+struct Simple_Unit_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
